97aba9ad6134cddcd616923c5a8055ad078b0d4e
Marcus Caisey <marcus@teckna.com>
2026-07-02 15:49:22 +0100

    feat: add results printing

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/marcuscaisey/playground
 
 go 1.26.2
+
+require (
+	github.com/fsnotify/fsnotify v1.10.1
+)
+
+require (
+	golang.org/x/sys v0.13.0 // indirect
+)
--- pg/main.go
+++ pg/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"cmp"
 	"context"
 	"embed"
 	"errors"
@@ -17,22 +18,48 @@ import (
 	"strings"
 	"syscall"
 	"time"
+
+	"github.com/fsnotify/fsnotify"
 )
 
 func main() {
-	os.Exit(cli())
+	os.Exit(cli(os.Args))
 }
 
-// cli parses the CLI args, runs pg, reports any errors, and returns the process exit code.
-//
+const resultsCmd = "__results"
+
+// cli parses its args, runs one of the session or results commands, and returns the corresponding
+// exit code.
+func cli(args []string) int {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM, syscall.SIGHUP)
+	defer stop()
+	if len(args) > 1 && args[1] == resultsCmd {
+		return resultsCLI(ctx, args[2:]) // Drop program name and command.
+	} else {
+		return sessionCLI(ctx, args[1:]) // Drop program name.
+	}
+}
+
+const usageErrorExitCode = 2
+
+// sessionCLI parses the args for the session command, runs a session, reports any errors, and
+// returns an exit code.
 // It returns 0 for success or help, 2 for incorrect CLI usage, and 1 for other errors.
-func cli() int {
+func sessionCLI(ctx context.Context, args []string) int {
 	// [flag.Parse] emits parsing errors without:
 	//   - "error: " before the error message
-	//   - A newline betweeen the error message and the usage text
-	// We use our own [flag.FlagSet] so that we can we can format the output how we want.
+	//   - A blank line betweeen the error message and the usage text
+	// We use our own [flag.FlagSet] so that we can format the output how we want.
 	flagSet := flag.NewFlagSet("pg", flag.ContinueOnError)
 	flagSet.SetOutput(io.Discard)
+
+	const (
+		editorEnvVar  = "EDITOR"
+		defaultEditor = "vi"
+	)
+	editorFlag := flagSet.String("editor", "", fmt.Sprintf("Editor to open; falls back to $%s, then %q.", editorEnvVar, defaultEditor))
+	printHelp := flagSet.Bool("help", false, "Print this message.")
+
 	usage := func() {
 		fmt.Fprintln(os.Stderr, "Usage: pg [options] <template-name>")
 		fmt.Fprintln(os.Stderr)
@@ -44,13 +71,10 @@ func cli() int {
 	usageErrorf := func(msg string, a ...any) int {
 		fmt.Fprintf(os.Stderr, "error: %s\n\n", fmt.Sprintf(msg, a...))
 		usage()
-		return 2
+		return usageErrorExitCode
 	}
 
-	editor := flagSet.String("editor", "", fmt.Sprintf("Editor to open; falls back to $%s, then %q.", editorEnvVar, defaultEditor))
-	printHelp := flagSet.Bool("help", false, "Print this message.")
-
-	if err := flagSet.Parse(os.Args[1:]); err != nil {
+	if err := flagSet.Parse(args); err != nil {
 		return usageErrorf("%s", err)
 	}
 
@@ -67,30 +91,45 @@ func cli() int {
 		return usageErrorf("unexpected arguments: %s", strings.Join(args[1:], ", "))
 	}
 
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM, syscall.SIGHUP)
-	defer stop()
-	if err := pg(ctx, templateName, *editor); err != nil {
-		fmt.Fprintf(os.Stderr, "error: %s\n", err)
-		return 1
+	pgPath, err := os.Executable()
+	if err != nil {
+		return errorExit(err)
+	}
+	editor := cmp.Or(*editorFlag, os.Getenv(editorEnvVar), defaultEditor)
+	if err := runSession(ctx, pgPath, templateName, editor); err != nil {
+		return errorExit(err)
 	}
 
 	return 0
 }
 
-const (
-	editorEnvVar  = "EDITOR"
-	defaultEditor = "vi"
-)
+// resultsCLI parses the args for the results command, prints the results for a session, reports any
+// errors, and returns an exit code.
+// It returns 0 for success or help, 2 for incorrect CLI usage, and 1 for other errors.
+func resultsCLI(ctx context.Context, args []string) int {
+	if len(args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: pg %s <session-dir> <entrypoint>\n", resultsCmd)
+		return usageErrorExitCode
+	}
+	sessionDir := args[0]
+	entrypoint := args[1]
+	if err := printSessionResults(ctx, sessionDir, entrypoint); err != nil {
+		return errorExit(err)
+	}
+	return 0
+}
 
-// TODO: fill in
-func pg(ctx context.Context, templateName string, editor string) (err error) {
-	// 1. Load template
-	// 2. Create session directory
-	// 3. CD to session directory
-	// 4. Split pane
-	// 5. Start nvim in top pane
-	// 6. Start pg in runner mode in bottom pane
+// errorExit reports an error and returns the general error exit code 1.
+func errorExit(err error) int {
+	fmt.Fprintf(os.Stderr, "error: %s\n", err)
+	return 1
+}
 
+// runSession runs a session using the given template.
+// It creates the session directory, copies the template files into it, starts the results command
+// in a new tmux pane, and opens the template entrypoint in the given editor.
+// pgPath must be the absolute path to the pg executable. This is used to start the results command.
+func runSession(ctx context.Context, pgPath string, templateName string, editor string) (err error) {
 	template, err := loadTemplate(templateName)
 	if err != nil {
 		if errors.Is(err, errTemplateNotFound) {
@@ -103,44 +142,100 @@ func pg(ctx context.Context, templateName string, editor string) (err error) {
 			}
 			return fmt.Errorf("%s is invalid: %s", templateSrc, templateInvalidErr.Reason)
 		}
-		return err
+		return fmt.Errorf("running session: %s", err)
 	}
 
 	sessionDir, err := anonSessionDir(template.Name)
 	if err != nil {
-		return err
+		return fmt.Errorf("running session: %s", err)
 	}
 	if err := setupSessionDir(sessionDir, template.FS); err != nil {
-		return err
-	}
-	if err := os.Chdir(sessionDir); err != nil {
-		return err
+		return fmt.Errorf("running session: %s", err)
 	}
 
-	closeResultsPane, err := runInNewTmuxPane(ctx, "watch date")
+	resultsCmd := fmt.Sprintf("%q %s %q %q", pgPath, resultsCmd, sessionDir, template.Entrypoint)
+	closeResultsPane, err := runInNewTmuxPane(ctx, resultsCmd)
 	if err != nil {
-		return err
+		return fmt.Errorf("running session: %s", err)
 	}
 	defer func() {
 		if closeErr := closeResultsPane(); closeErr != nil {
-			err = errors.Join(err, fmt.Errorf("closing results pane: %s", closeErr))
+			err = errors.Join(err, fmt.Errorf("running session: closing results pane: %s", closeErr))
 		}
 	}()
 
-	editor = cmp.Or(editor, os.Getenv(editorEnvVar), defaultEditor)
 	editorCmd := exec.CommandContext(ctx, editor, template.Entrypoint)
+	editorCmd.Dir = sessionDir
 	editorCmd.Stdin = os.Stdin
 	editorCmd.Stdout = os.Stdout
 	editorCmd.Stderr = os.Stderr
 	if err := editorCmd.Run(); err != nil {
-		return fmt.Errorf("running %q: %s", editor, cmdErrMsg(err))
+		return fmt.Errorf("running session with editor %q: %s", editor, cmdErrMsg(err))
 	}
 
 	return nil
 }
 
+// printSessionResults watches a session directory for changes to the entrypoint, executing the
+// run script and printing the results when changes occur.
+func printSessionResults(ctx context.Context, sessionDir string, entrypoint string) (err error) {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		return fmt.Errorf("printing session results: %s", err)
+	}
+	defer func() {
+		if closeErr := watcher.Close(); closeErr != nil {
+			err = errors.Join(err, fmt.Errorf("printing session results: closing session directory watcher: %s", closeErr))
+		}
+	}()
+
+	if err := watcher.Add(sessionDir); err != nil {
+		return fmt.Errorf("printing session results: adding session directory watcher: %s", err)
+	}
+
+	entrypointPath := filepath.Join(sessionDir, entrypoint)
+	var startTimerC <-chan time.Time
+	stopCurrentRun := func() {}
+	defer stopCurrentRun()
+	for {
+		select {
+		case event, ok := <-watcher.Events:
+			if !ok {
+				return fmt.Errorf("printing session results: watcher events channel closed")
+			}
+			if event.Name != entrypointPath {
+				continue
+			}
+			if !event.Has(fsnotify.Write) && !event.Has(fsnotify.Create) {
+				continue
+			}
+
+			const debounceDuration = 100 * time.Millisecond
+			startTimerC = time.After(debounceDuration)
+
+		case <-startTimerC:
+			stopCurrentRun()
+			stopCurrentRun, err = startRunScript(ctx, sessionDir, entrypoint)
+			if err != nil {
+				return fmt.Errorf("printing session results: %s", err)
+			}
+
+		case <-ctx.Done():
+			return fmt.Errorf("printing session results: %s", ctx.Err())
+
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				return fmt.Errorf("printing session results: watcher error channel closed")
+			}
+			return fmt.Errorf("printing session results: watching session directory: %s", err)
+		}
+	}
+}
+
 //go:embed templates/*
 var builtinTemplates embed.FS
+
+const runScriptFilename = "run.sh"
 
 // template represents a playground template.
 //
@@ -217,7 +312,6 @@ func loadTemplate(name string) (template, error) {
 		return template{}, fmt.Errorf("loading template %q: %w", name, templateInvalidErr)
 	}
 
-	const runScriptFilename = "run.sh"
 	if _, err := fs.Stat(templateFS, runScriptFilename); err != nil {
 		if os.IsNotExist(err) {
 			templateInvalidErr := newTemplateInvalidErrorf(name, "", "run script (%q file) is missing", runScriptFilename)
@@ -233,31 +327,119 @@ func loadTemplate(name string) (template, error) {
 	}, nil
 }
 
+func anonSessionDir(templateName string) (string, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", fmt.Errorf("generating anonymous session directory name for template %q: %s", templateName, err)
+	}
+	sessionName := fmt.Sprintf("%s-%s", templateName, time.Now().Format(fmt.Sprintf("%s-%s", time.DateOnly, time.TimeOnly)))
+	return filepath.Join(homeDir, ".pg", "sessions", "anonymous", sessionName), nil
+}
+
+// setupSessionDir creates the directory for a session and copies the template files into it.
+func setupSessionDir(dir string, templateFS fs.FS) error {
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return fmt.Errorf("setting up session directory: %s", err)
+	}
+	if err := os.CopyFS(dir, templateFS); err != nil {
+		return fmt.Errorf("setting up session directory: %s", err)
+	}
+	return nil
+}
+
 // runInNewTmuxPane splits the current tmux pane vertically and runs a command in the new pane,
 // leaving the current pane selected.
 // The returned function closes the new pane.
 func runInNewTmuxPane(ctx context.Context, cmd string) (func() error, error) {
 	paneID, ok := os.LookupEnv("TMUX_PANE")
 	if !ok {
-		return nil, fmt.Errorf("splitting tmux window: not currently in a tmux session")
+		return nil, fmt.Errorf("running %q in new tmux pane: not currently in a tmux session", cmd)
 	}
 
 	tmuxCmd := exec.CommandContext(ctx, "tmux", "split-window", "-t", paneID, "-d", "-P", "-F", "#{pane_id}", cmd)
 	output, err := tmuxCmd.Output()
 	if err != nil {
-		return nil, fmt.Errorf("splitting tmux window: %s", cmdErrMsg(err))
+		return nil, fmt.Errorf("running %q in new tmux pane: splitting tmux window: %s", cmd, cmdErrMsg(err))
 	}
 	newPaneID := strings.TrimSpace(string(output))
 
 	return func() error {
-		// We don't propagate the context to this command since we want to make sure that it can run
-		// even if the context has been cancelled.
+		// We don't propagate the context to this command since we want it to run even if the
+		// context gets cancelled.
 		cmd := exec.Command("tmux", "kill-pane", "-t", newPaneID)
 		if _, err = cmd.Output(); err != nil {
 			return fmt.Errorf("killing tmux pane %q: %s", newPaneID, cmdErrMsg(err))
 		}
 		return nil
 	}, nil
+}
+
+// startRunScript starts execution of a session's run script and returns a function to stop it.
+// The run script is executed as "bash run.sh $entrypoint" from the session directory.
+// The returned function blocks until execution has been stopped and is safe to be called after
+// execution has stopped.
+func startRunScript(ctx context.Context, sessionDir string, entrypoint string) (stop func(), err error) {
+	cmdCtx, cancelCmdCtx := context.WithCancel(ctx)
+	defer func() {
+		if err != nil {
+			cancelCmdCtx()
+		}
+	}()
+
+	cmd := exec.CommandContext(cmdCtx, "bash", runScriptFilename, entrypoint)
+	cmd.Dir = sessionDir
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	// Run bash in a process group so that we can kill it and any child processes spawned by run.sh.
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+	cmd.Cancel = func() error {
+		return syscall.Kill(-cmd.Process.Pid, syscall.SIGKILL)
+	}
+
+	fmt.Print(ansiClearScreen)
+	fmt.Print(ansiMoveCursorHome)
+	styledPrintf(ansiBoldGreen, "Executing %q\n\n", cmd.String())
+	if err := cmd.Start(); err != nil {
+		return nil, fmt.Errorf("executing %q in %q: %s", entrypoint, sessionDir, err)
+	}
+
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+		exitStatus := 0
+		exitStatusStyle := ansiBoldGreen
+		if err := cmd.Wait(); err != nil {
+			exitErr, ok := errors.AsType[*exec.ExitError](err)
+			if !ok {
+				return
+			}
+			exitStatus = exitErr.ExitCode()
+			exitStatusStyle = ansiBoldRed
+		}
+		styledPrintf(exitStatusStyle, "\nExited with status %d\n", exitStatus)
+	}()
+
+	return func() {
+		cancelCmdCtx()
+		<-done
+	}, nil
+}
+
+type style string
+
+const (
+	ansiClearScreen    style = "\x1b[2J"
+	ansiMoveCursorHome style = "\x1b[H"
+	ansiBoldGreen      style = "\x1b[1;32m"
+	ansiBoldRed        style = "\x1b[1;31m"
+	ansiReset          style = "\x1b[0m"
+)
+
+// styledPrintf prints text styled using the given ANSI escape sequence(s).
+func styledPrintf(style style, format string, a ...any) {
+	fmt.Printf("%s%s%s", style, fmt.Sprintf(format, a...), ansiReset)
 }
 
 // cmdErrMsg returns the appropriate error message for an [error] returned by [exec.Cmd.Output].
@@ -268,25 +450,4 @@ func cmdErrMsg(err error) string {
 		return string(bytes.TrimSpace(exitErr.Stderr))
 	}
 	return err.Error()
-}
-
-func anonSessionDir(templateName string) (string, error) {
-	homeDir, err := os.UserHomeDir()
-	if err != nil {
-		return "", fmt.Errorf("generating anonymous session directory name: %s", err)
-	}
-	sessionName := fmt.Sprintf("%s-%s", templateName, time.Now().Format(fmt.Sprintf("%s-%s", time.DateOnly, time.TimeOnly)))
-	return filepath.Join(homeDir, ".pg", "sessions", "anonymous", sessionName), nil
-}
-
-// setupSessionDir sets up the directory for a session by creating it and copying the template files
-// to the directory.
-func setupSessionDir(dir string, fs fs.FS) error {
-	if err := os.MkdirAll(dir, 0755); err != nil {
-		return fmt.Errorf("setting up anonymous session directory: %s", err)
-	}
-	if err := os.CopyFS(dir, fs); err != nil {
-		return fmt.Errorf("setting up anonymous session directory: %s", err)
-	}
-	return nil
 }
